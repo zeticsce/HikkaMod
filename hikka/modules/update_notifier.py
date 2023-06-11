d260dc8ff8ea51c24b3b06bfbd09ec244ba3dfe1
zeticsce <63130611+zeticsce@users.noreply.github.com>
2023-06-11 21:38:38 +0300

    Update update_notifier.py

--- hikka/modules/update_notifier.py
+++ hikka/modules/update_notifier.py
@@ -97,7 +97,7 @@ class UpdateNotifierMod(loader.Module):
                 "https://t.me/hikari_assets/71",
                 caption=self.strings("update_required").format(
                     utils.get_git_hash()[:6],
-                    '<a href="https://github.com/hikariatama/Hikka/compare/{}...{}">{}</a>'
+                    '<a href="https://github.com/zeticsce/HikkaMod/compare/{}...{}">{}</a>'
                     .format(
                         utils.get_git_hash()[:12],
                         self.get_latest()[:12],
