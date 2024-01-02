9425f5b016f9b93f6a43ef89212b9ece6b3bcc29
vlad <vladrudovol06@gmail.com>
2024-01-03 03:45:49 +0600

    database.py

--- hikka/database.py
+++ hikka/database.py
@@ -189,7 +189,11 @@ class Database(dict):
 
         return True
 
-    def save(self) -> bool:
+    def save(self):
+        asyncio.ensure_future(self._save())
+        return
+
+    async def _save(self) -> bool:
         """Save database"""
         if not self.process_db_autofix(self):
             try:
